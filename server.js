0b3558e425f6cb4670d6f699a21b10af1b1365df
IhorMaksymov <bonefadze0ne@gmail.com>
2023-03-21 19:12:03 +0200

    db host

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
-// const { DB_HOST, PORT = 5000 } = process.env;
-const DB_HOST = 'mongodb+srv://Bonefadze:0987654321@cluster0.wt7yyxq.mongodb.net/db-contacts?retryWrites=true&w=majority'
-const PORT = 5000;
+// https://gks-veteran.onrender.com
+const { DB_HOST, PORT = 5000 } = process.env;
 
 const app = require('./app');
 
