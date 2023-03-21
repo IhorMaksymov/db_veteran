0565d6de42f44fb341499607988b25ab032b8cb0
IhorMaksymov <bonefadze0ne@gmail.com>
2023-03-21 18:19:34 +0200

    start backend

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const { DB_HOST, PORT = 5000 } = process.env;
+// const { DB_HOST, PORT = 5000 } = process.env;
+const DB_HOST = 'mongodb+srv://Bonefadze:0987654321@cluster0.wt7yyxq.mongodb.net/db-contacts?retryWrites=true&w=majority'
+const PORT = 5000;
 
 const app = require('./app');
+
+mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
   .then(() => app.listen(PORT, () => console.log(`Database connection port ${PORT} successful`)))
