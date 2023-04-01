caf768a748903dc7f3b8d6b0558930b50dc1cb36
IhorMaksymov <bonefadze0ne@gmail.com>
2023-04-01 13:38:24 +0300

    test token

--- controllers/adminControl/login.js
+++ controllers/adminControl/login.js
@@ -20,9 +20,9 @@ const login = async (req, res) => {
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '24h' });
     await Admin.findByIdAndUpdate(user._id, { token });
     res.status(200).json({
-        token,
         user: {
             email,
+            token,
         }
     })
 }
--- controllers/adminControl/signup.js
+++ controllers/adminControl/signup.js
@@ -1,8 +1,11 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { Admin } = require('../../models/adminSchema');
 
 const { ctrlWrapper, HttpError } = require('../../helpers');
+
+const { SECRET_KEY } = process.env;
 
 const signup = async (req, res) => {
     const { email, password } = req.body;
@@ -13,13 +16,19 @@ const signup = async (req, res) => {
 
     const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-    await Admin.create({ email, password: hashPass });
+    const newUser =  await Admin.create({ email, password: hashPass, });
+
+    // const token = jwt.sign({newUser}, SECRET_KEY, { expiresIn: '24h' });
+
+    const token = jwt.sign({ id: newUser._id }, SECRET_KEY, { expiresIn: '24h' });
+    
     res.status(201).json({
         status: 'success',
         code: 201,
         data: {
             user: {
                 email,
+                token,
             }
         }
     });
